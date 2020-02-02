b578c23f35670fdd73406f978f2b830f2db2f5cc
Jonathan Higger <jonathan.higger@gmail.com>
2020-02-02 05:49:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,12 +9,7 @@ def map(array)
   return new
 end
 
-def reduce(array,sp=nil)
- if sp
-   t=sp
- else
-   t=0
- end
+def reduce(array,t==0)
   i=0
  while i<array.length do
       t=t+yield(array[i])
